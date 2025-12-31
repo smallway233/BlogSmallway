30a5b3498732e08c8b19a1db91b7af68b85541ce
小白nbfan <>
2025-12-31 14:15:13 +0800

    docs: 修改文档基础路径为/BlogSmallway/
    修改VuePress配置中的base路径，以适配项目部署需求

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/BlogSmallway/',
   lang: 'zh-CN',
   locales: {
     '/': {
